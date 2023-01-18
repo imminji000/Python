0a6caed0f2fe36a8df6ad88b54dffbd9cee6aef6
imminji000 <swlss79@naver.com>
2023-01-18 13:14:51 +0900

    2023/01/18

---
+++ ch08/2.사용자 관리.py
@@ -0,0 +1,52 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 2,
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "\"\"\"\n",
+    "날짜 : 2023/01/13\n",
+    "이름 : 임민지\n",
+    "내용 : 파이썬 사용자 관리 프로그램 실습\n",
+    "\"\"\"\n",
+    "import pymysql"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.11.1"
+  },
+  "orig_nbformat": 4,
+  "vscode": {
+   "interpreter": {
+    "hash": "ca3167030a8377a7b82d5e665b459d458267ba920e721652be188ae573e682bb"
+   }
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 2
+}
